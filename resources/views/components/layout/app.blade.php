c5c03d6f2084410d5cfe18c3548b5875aded2832
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2026-05-27 14:36:23 +0700

    Update #2.3 Homepage Update

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title> ISFEST 2026 | {{ $title }}</title>
   
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link href="https://fonts.googleapis.com/css2?family=Amarante&family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
